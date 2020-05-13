2fec3c0c01f226e36f3536518a041ee00293e4a8
Ashish Pradhan <ssashish21@gmail.com>
2020-05-13 16:43:43 +0530

    test

--- app.js
+++ app.js
@@ -1,59 +1,68 @@
 
 const express = require("express");
-const router = express.Router();
-const bodyParser = require("body-parser");
-const ejs = require("ejs");
+const expressLayouts = require('express-ejs-layouts');
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const flash = require('express-flash');
+const ejs = require("ejs");
+const Session = require("express-session");
+const passport = require("passport");
 
-mongoose.connect("mongodb://localhost:27017/laptechDB" ,{ useNewUrlParser: true , useUnifiedTopology: true });
-
-
-
-// EJS
 const app = express();
-app.set("view engine","ejs");
 
-const PORT = process.env.PORT || 5000;
+//passport config
+require('./config/passport')(passport);
 
-var path = require("path");
+//connect to mongo
+var MongoClient = require('mongodb').MongoClient;
 
-//Bodyparser
-app.use(bodyParser.urlencoded({extended:false}));
-
-
-app.use(express.static('public')); // access public File
-
-
-
-
-
-
-//homepage
-app.get("/",(req,res) => res.render("index"));
-
-//login page
-app.get("/signin",(req,res) => res.render("signin"));
-
-//registration page
-app.get("/signup",(req,res) => res.render("signup"));
-
-//register handle
-app.post("/signup",(req,res) => {
-  console.log(req.body);
-  res.send('hello');
+// Connect to the db
+MongoClient.connect("mongodb://localhost:27017/laptechDB",{ useNewUrlParser: true , useUnifiedTopology: true }, function (err, db) {
+     if(err) throw err;
+     else console.log("mongoDB connected..");
 });
 
+// access public File
+app.use(express.static('public'));
 
-//user dashboard
-app.get("/dashboard",(req,res)=>res.render("dashboard"));
+//EJS
+app.use(expressLayouts);
+app.set("view engine","ejs");
+
+//Bodyparser
+ app.use(bodyParser.urlencoded({extended:true}));
+
+ //Express Session
+ app.use(
+     Session({
+     secret: "This is a bloging app",
+     resave: false,
+     saveUninitialized: false
+     })
+ );
+
+ //passport middleware
+ app.use(passport.initialize());
+ app.use(passport.session());
+
+ //connect  flash
+ app.use(flash());
+
+ //global variable
+ app.use(function(req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+  });
+
+// Routes
+app.use('/',require('./routes/index'));
+app.use('/users',require('./routes/users'));
+
+// var path = require("path");
 
 
-
-
-
-
-
-
-
+const PORT = process.env.PORT || 5555 ;
 
 app.listen(PORT,console.log('server running on port '+ PORT));
